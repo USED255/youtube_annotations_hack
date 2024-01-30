f4214abba55c2accc239682b62a01757403ee93e
我如天翼 <wrtyis@outlook.com>
2024-01-30 22:50:17 +0800

    更新

--- server.py
+++ server.py
@@ -1,21 +1,23 @@
 from flask import Flask, send_from_directory
 
+web = "web"
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return send_from_directory(".", "iCkYw3cRwLo.html")
+    return send_from_directory(web, "iCkYw3cRwLo.html")
 
 
 @app.route("/annotations_invideo", methods=["GET", "POST"])
 def annotations():
-    return send_from_directory(".", "iCkYw3cRwLo.xml")
+    return send_from_directory(web, "iCkYw3cRwLo.xml")
 
 
 @app.route("/<path:filename>")
 def get_file(filename):
-    return send_from_directory(".", filename)
+    return send_from_directory(web, filename)
 
 
 if __name__ == "__main__":
